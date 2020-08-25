48f1b1bb44d0a2c70a6b01c0f35d7d11a18dfad5
Christoph Coijanovic <mail@coijanovic.com>
2020-08-25 15:26:33 +0200

    added battery indicator to statusbar

---
+++ .config/sway/status.sh
@@ -0,0 +1,13 @@
+# The Sway configuration file in ~/.config/sway/config calls this script.
+# You should see changes to the status bar after saving this script.
+# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
+
+# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
+# like 2018-10-06 and the time (e.g., 14:01)
+date_formatted=$(date "+%a %F %H:%M")
+
+# Returns the battery status: "Full", "Discharging", or "Charging".
+battery_status=$(acpi | cut -d " " -f 4-)
+
+# Emojis and characters for the status bar
+echo  $battery_status "🔋|" $date_formatted
